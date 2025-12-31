164cf498768ea946bc9cfe45bd17fa0c082c5c45
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2025-12-31 14:53:44 +0530

    Home component temp design

---
+++ src/app/core/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  isCollapsed = true;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+}
