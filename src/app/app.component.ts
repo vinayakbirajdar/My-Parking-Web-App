90ca6e0c07021952d446320a3aa99036b8caa371
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2026-01-01 23:27:33 +0530

    Icon installation
    replace all icon with angular core material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule],
   template: `<router-outlet></router-outlet>`
 })
 export class AppComponent { }
--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
