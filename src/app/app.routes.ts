3d3f179009e0731e58f6e53a1afcb3f36980cc43
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2026-01-02 23:01:21 +0530

    Routing for all side menu implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { LoginComponent } from './screens/login/login.component';
 import { HomeComponent } from './screens/home/home.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { ProfileComponent } from './screens/profile/profile.component';
+import { WalletComponent } from './screens/wallet/wallet.component';
+import { PrivacySecurityComponent } from './screens/privacy-security/privacy-security.component';
+import { HelpComponent } from './screens/help/help.component';
+import { AboutComponent } from './screens/about/about.component';
 
 export const routes: Routes = [
     // AUTH LAYOUT
@@ -19,7 +24,12 @@ export const routes: Routes = [
         path: '',
         component: MainLayoutComponent,
         children: [
-            { path: 'home', component: HomeComponent }
+            { path: 'home', component: HomeComponent },
+            { path: 'profile', component: ProfileComponent },
+            { path: 'wallet', component: WalletComponent },
+            { path: 'privacySecurity', component: PrivacySecurityComponent },
+            { path: 'help', component: HelpComponent },
+            { path: 'about', component: AboutComponent }
         ]
     },
     // FALLBACK
--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule, MatIcon, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
